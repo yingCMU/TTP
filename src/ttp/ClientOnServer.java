936d07f255e9867de6894b8a7a2d76d9bd71b8af
yingCMU <ying.li@sv.cmu.edu>
2013-04-21 16:22:36 -0400

    sunday 4

--- src/ttp/ClientOnServer.java
+++ src/ttp/ClientOnServer.java
@@ -1,5 +1,25 @@
 package ttp;
 
 public class ClientOnServer {
-	private int hisSYN;
+	private int SYN;
+	private int ACK;
+	private ConDescriptor dp;
+	public int getSYN() {
+		return SYN;
+	}
+	public void setSYN(int sYN) {
+		SYN = sYN;
+	}
+	public int getACK() {
+		return ACK;
+	}
+	public void setACK(int aCK) {
+		ACK = aCK;
+	}
+	public ConDescriptor getDp() {
+		return dp;
+	}
+	public void setDp(ConDescriptor dp) {
+		this.dp = dp;
+	}
 }
